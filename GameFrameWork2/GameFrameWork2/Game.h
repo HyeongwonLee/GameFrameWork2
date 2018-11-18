15c712a3efe083a84bb226fd4693067b1a6d0f30
HyeongwonLee <42854855+HyeongwonLee@users.noreply.github.com>
2018-11-18 14:55:09 +0900

    PP07.TextureManager

---
+++ GameFrameWork2/GameFrameWork2/Game.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "SDL.h"
+#include <SDL_image.h>
+#include <iostream>
+#include "TextureManager.h"
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+
+	void render();
+	void update();
+	void handleEvent();
+	void clean();
+	bool running() { return m_bRunning; }
+private:
+	//SDL_Texture * m_pTexture;
+	//SDL_Rect m_sourceRectangle;
+	//SDL_Rect m_destinationRectangle;
+
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+
+	int m_currentFrame;
+	TextureManager m_textureManager;
+};
