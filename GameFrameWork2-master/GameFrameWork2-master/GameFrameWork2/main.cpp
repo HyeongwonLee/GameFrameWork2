7ca30bc93fff2a80f323c5104ce4259b402f7d3f
HyeongwonLee <42854855+HyeongwonLee@users.noreply.github.com>
2018-11-18 14:51:26 +0900

    PP07.TextureManager

---
+++ GameFrameWork2-master/GameFrameWork2-master/GameFrameWork2/main.cpp
@@ -0,0 +1,20 @@
+#include<SDL.h>
+#include "Game.h"
+
+Game* g_game = 0;
+
+int main(int argc, char* args[])
+{
+	g_game = new Game();
+	g_game->init("Chapter 1,",100, 100, 640, 480, false);
+	
+	while (g_game->running())
+	{
+		g_game->handleEvent();
+		g_game->update();
+		g_game->render();
+	}
+
+	g_game->clean();
+	return 0;
+}
